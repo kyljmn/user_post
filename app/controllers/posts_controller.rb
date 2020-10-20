fbdeca38358f1d7cf8785389f4e7cbfed7c162e8
kyljmn <kylejemino@gmail.com>
2020-10-20 18:26:23 +0800

    fixed form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,25 @@
 class PostsController < ApplicationController
-  before_action :user_from_param, only: [:create, :delete]
+  before_action :user_from_param, only: [:create, :destroy]
+  
   def create
     @post = @user.posts.create(posts_param)
     redirect_to user_path(@user)
   end
+  
   def destroy
     @post = @user.posts.find(params[:id])
     @post.destroy
     redirect_to user_path(@user)
   end
+  
   private
+
     def posts_param
       params.require(:post).permit(:title, :body)
     end
+    
     def user_from_param
       @user = User.find(params[:user_id])
     end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @post = Post.new
   end
 
   def new
